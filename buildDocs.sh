f1ee4c6db98ac6d6988b63e0d7f23661a2fb9f22
Jonatas Macedo <jonatasmacedo@rubeus.com.br>
2019-09-17 17:15:25 -0300

    Update site

--- buildDocs.sh
+++ buildDocs.sh
@@ -3,7 +3,7 @@ git add .
 git commit -a -m 'Update site'
 git push
 git checkout master
-python /home/$USER/.local/bin/mkdocs build -f mkdocs.yml
+mkdocs build -f mkdocs.yml
 cp -r site ../site
 git checkout gh-pages
 cp -r ../site/. ./
